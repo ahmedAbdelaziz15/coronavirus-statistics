3f480995ea2ce6e4505c7a18473355370038fa17
Yehia <ahmedyehia93@gmail.com>
2020-04-10 00:38:38 +0200

    Add bie chart compnent

--- src/app.js
+++ src/app.js
@@ -1,26 +1,39 @@
 import React, { Component } from "react";
 
 import { Cards, CountryPicker, Chart } from "./components";
+import coronaImage from "./assets/image.png";
+
 import { fetchData } from "./api";
 import styles from "./App.module.css";
 
 class App extends Component {
   state = {
     data: {},
+    selectedCountry: "",
   };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
-  render() {
-    const { data } = this.state;
 
+  handleCountryChange = async (country) => {
+    const fetchedData = country && (await fetchData(country));
+    this.setState({
+      data: fetchedData,
+      selectedCountry: country,
+    });
+  };
+
+  render() {
+    const { data, selectedCountry } = this.state;
     return (
       <div className={styles.container}>
+        {/* <CoronaImage /> */}
+        <img src={coronaImage} alt="COVID-19" className={styles.coronaImage} />
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker onCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={selectedCountry} />
       </div>
     );
   }
